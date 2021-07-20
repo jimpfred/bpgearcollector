8d821b9f2f73664188dea923de869692a91e6f1d
jimpfred <jimpfred@yahoo.com>
2021-07-20 16:35:38 -0700

    1 to M capability added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Hiker, Gear
+from .models import Hiker, Gear, ArrDepDate
 
 admin.site.register(Hiker)
 admin.site.register(Gear)
+admin.site.register(ArrDepDate)
+
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.urls import reverse
 
+TYPE = (
+    ('A', 'Park Arrival'),
+    ('D', 'Park Departure'),
+)
 # Create your models here.
 
 class Gear(models.Model):
@@ -24,3 +28,14 @@ class Hiker(models.Model):
   def get_absolute_url(self):
     return reverse('detail', kwargs={'hiker_id': self.id})
 
+class ArrDepDate(models.Model):
+  date = models.DateField('arrival or departure date')
+  type = models.CharField(
+    max_length=1,
+    choices=TYPE,
+    default=TYPE[0][0]
+  )
+  hiker = models.ForeignKey(Hiker, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_type_display()} on {self.date}"
